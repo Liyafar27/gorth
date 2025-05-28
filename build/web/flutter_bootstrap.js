1235adafd9a296868e0144902cff2282894bc1f0
aliyafarkhshatova <liyafarapp@gmail.com>
2025-05-28 15:10:44 +0500

    “v9”

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3346851812"
+    serviceWorkerVersion: "1818866691"
   }
 });
